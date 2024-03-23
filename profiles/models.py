1b35c670504b3fcbed6b956cde0a75f0112c7808
“Lukasz <lukasz.glowacz@glowacz.se>
2024-03-23 13:05:22 +0100

    Added profile model with additional field to user default fileds. Added first name, last name, perssonnummer with regex, image field and created and updated datetimefields.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import RegexValidator
 
-# Create your models here.
+personnummer_regex = RegexValidator(regex=r'^\d{6}-\d{4}$', message='XXXXXX-XXXX')
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    personnummer = models.CharField(max_length=11, unique=True, validators=[personnummer_regex])
+    image = models.ImageField(upload_to='images/', default='../default_profile_l2i70s')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+    
+# Tworzenie profilu uzytkownika po utworzeniu konta
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+# Aktualizacja profilu po zapisaniu uzytkownika
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
