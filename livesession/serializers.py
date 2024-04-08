6804c52b3ec54c32edefcae8fef449d373288fcd
“Lukasz <lukasz.glowacz@glowacz.se>
2024-04-08 15:04:25 +0200

    Updated LiveSession Serializer and added user first name, last name and workplace detail view to the API endpoint.

--- livesession/serializers.py
+++ livesession/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import LiveSession
 
 class LiveSessionSerializer(serializers.ModelSerializer):
+    user_first_name = serializers.CharField(source='user.profile.first_name', read_only=True)
+    user_last_name = serializers.CharField(source='user.profile.last_name', read_only=True)
+    workplace_detail = serializers.SerializerMethodField()
+
     class Meta:
         model = LiveSession
-        fields = ['id', 'user', 'workplace', 'start_time', 'status']
+        fields = ['id', 'user', 'workplace', 'start_time', 'status', 'user_first_name', 'user_last_name', 'workplace_detail']
+
+    def get_workplace_detail(self, obj):
+        return f"{obj.workplace.street} {obj.workplace.street_number}, {obj.workplace.postal_code} {obj.workplace.city}"
